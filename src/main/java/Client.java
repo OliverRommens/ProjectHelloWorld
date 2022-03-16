3afe6e9ee0ed7559103e6b3aa9450c8c1aa30882
jeoffreycanters <jeoffrey.canters@student.uantwerpen.be>
2022-03-16 12:38:22 +0100

    TCP + file

--- src/main/java/Client.java
+++ src/main/java/Client.java
@@ -1,27 +1,38 @@
 import java.io.*;
 import java.net.Socket;
-import java.util.Scanner;
 
 public class Client {
     private static DataOutputStream dataOutputStream = null;
     private static DataInputStream dataInputStream = null;
-    private static Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) {
-        try(Socket socket = new Socket("localhost",5000)){
+        try(Socket socket = new Socket("localhost",5000)) {
             dataInputStream = new DataInputStream(socket.getInputStream());
             dataOutputStream = new DataOutputStream(socket.getOutputStream());
 
-            while (true) {
-                System.out.print("input> ");
-                String message = scanner.nextLine();
-                dataOutputStream.writeUTF(message);
-                if(message.equalsIgnoreCase("exit()"))
-                    break;
-            }
+            sendFile("C:\\Users\\jeoff\\IdeaProjects\\ProjectHelloWorld2\\src\\main\\java\\text");
+            sendFile("C:\\Users\\jeoff\\IdeaProjects\\ProjectHelloWorld2\\src\\main\\java\\text2");
 
+            dataInputStream.close();
+            dataInputStream.close();
         }catch (Exception e){
-            System.out.println(e.toString());
+            e.printStackTrace();
         }
+    }
+
+    private static void sendFile(String path) throws Exception{
+        int bytes = 0;
+        File file = new File(path);
+        FileInputStream fileInputStream = new FileInputStream(file);
+
+        // send file size
+        dataOutputStream.writeLong(file.length());
+        // break file into chunks
+        byte[] buffer = new byte[4*1024];
+        while ((bytes=fileInputStream.read(buffer))!=-1){
+            dataOutputStream.write(buffer,0,bytes);
+            dataOutputStream.flush();
+        }
+        fileInputStream.close();
     }
 }
--- src/main/java/Server.java
+++ src/main/java/Server.java
@@ -1,4 +1,6 @@
-import java.io.*;
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.FileOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -10,21 +12,31 @@ public class Server {
         try(ServerSocket serverSocket = new ServerSocket(5000)){
             System.out.println("listening to port:5000");
             Socket clientSocket = serverSocket.accept();
-            System.out.println(clientSocket+" connected\n");
+            System.out.println(clientSocket+" connected.");
             dataInputStream = new DataInputStream(clientSocket.getInputStream());
             dataOutputStream = new DataOutputStream(clientSocket.getOutputStream());
 
-            String message;
-            while (true) {
-                message = dataInputStream.readUTF();
-                System.out.println(message);
-                if(message.equalsIgnoreCase("exit()"))
-                    break;
-            }
-            clientSocket.close();
+            receiveFile("text");
+            receiveFile("text2");
 
+            dataInputStream.close();
+            dataOutputStream.close();
+            clientSocket.close();
         } catch (Exception e){
-            System.out.println(e.toString());
+            e.printStackTrace();
         }
+    }
+
+    private static void receiveFile(String fileName) throws Exception{
+        int bytes = 0;
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName);
+
+        long size = dataInputStream.readLong();     // read file size
+        byte[] buffer = new byte[4*1024];
+        while (size > 0 && (bytes = dataInputStream.read(buffer, 0, (int)Math.min(buffer.length, size))) != -1) {
+            fileOutputStream.write(buffer,0,bytes);
+            size -= bytes;      // read upto file size
+        }
+        fileOutputStream.close();
     }
 }
