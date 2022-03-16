09a57752d7ea69f3a70aa11b330d637d62746fa7
jeoffreycanters <jeoffrey.canters@student.uantwerpen.be>
2022-03-16 11:55:34 +0100

    hello Oliver

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, jeoffrey!");
-        System.out.println("Hello,revert this message");
-        System.out.println("Hello,revert this message2");
+        System.out.println("Hello Oliver!");
     }
 }
