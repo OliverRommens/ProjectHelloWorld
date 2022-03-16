d0aacf5089e56125000627af12b03fc9e10221e8
jeoffreycanters <jeoffrey.canters@student.uantwerpen.be>
2022-03-16 12:05:45 +0100

    hello Oliver

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,5 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, jeoffrey!");
         System.out.println("Hello Oliver!");
+        System.out.println("merge conflict");
     }
 }
